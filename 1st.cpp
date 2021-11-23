86a06758f90905bf6ef4c6c96d9aa63e17cc968b
abdulhamid100 <ahmadhamid040@gmail.com>
2021-11-23 14:30:51 +0600

    last one

---
+++ 1st.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+   
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+
+
+    cout<<"Hello I am Hamid"<<endl;
+    cout<<"This is 1st repository\n";
+
+
+
+    return 0;
+}
